fc8ddcae557630b18e14d754aabf80b12d61f441
fbastier <francois.bastier@gmail.com>
2021-06-04 12:44:15 +0200

    Commit initial

---
+++ src/Launcher.java
@@ -0,0 +1,8 @@
+public class Launcher {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome aboard !");
+        System.out.println("\nWhat do you want to do ?");
+
+    }
+}
